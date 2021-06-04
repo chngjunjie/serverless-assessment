7b32f2897d4082449d1a9c161ae9f869428b799a
Allen <chng_999@hotmail.com>
2021-06-04 11:02:22 +0800

    Completed for api, and unfinish test.

---
+++ __tests__/handler.test.js
@@ -0,0 +1,12 @@
+// Using Jest
+// handler.js
+function getUsersByName(language) {
+    // const handler = require('../index');
+
+    // test('correct greeting is generated', () => {
+    //   expect(handler.getLocalGreeting("en")).toBe("Hello!");
+    //   expect(handler.getLocalGreeting("fr")).toBe("🌊");
+    // });
+}
+module.exports.getLocalGreeting = getLocalGreeting;
+
